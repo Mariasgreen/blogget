ed0e9783afc9225cef90316c9af20861fccff531
mariiasgreen <mariiaz555@gmail.com>
2023-08-13 22:13:17 +0300

    done

---
+++ src/components/Header/Heading/Heading.jsx
@@ -0,0 +1,10 @@
+import style from './Heading.module.css';
+import PropTypes from 'prop-types';
+
+export const Heading = ({text}) => (
+  <p className={style.heading}>{text}</p>
+);
+
+Heading.propTypes = {
+  text: PropTypes.string.isRequired,
+};
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,11 @@
+import style from './Main.module.css';
+import Layout from '../Layout';
+
+export const Main = () => (
+  <main className={style.main}>
+    <Layout />
+  </main>
+);
+
+
+export default Main;
