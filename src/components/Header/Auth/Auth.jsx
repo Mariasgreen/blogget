c62d176015ecbb87138b1fcf960822d8f0466e80
mariiasgreen <mariiaz555@gmail.com>
2023-08-30 18:12:43 +0300

    tofix

--- src/components/Header/Auth/Auth.jsx
+++ src/components/Header/Auth/Auth.jsx
@@ -1,9 +1,20 @@
 /* eslint-disable max-len */
 import style from './Auth.module.css';
+import PropTypes from 'prop-types';
 import {ReactComponent as AuthIcon} from './img/login.svg';
+import {urlAuth} from '../../../api/auth';
+import {Text} from '../../../UI/Text/Text';
 
-export const Auth = () => (
+export const Auth = ({auth}) => (
   <button className={style.button} >
+    { auth ? (auth) :
+  (<Text As = 'a' href = {urlAuth}>
     <AuthIcon width={35} height={35} color='orange'/>
+  </Text>
+     )}
   </button>
 );
+
+Auth.propTypes = {
+  auth: PropTypes.bool,
+};
